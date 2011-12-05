ae3f849c9c287de12fe564b3a6e4fc5fa879cf8c
johannes.faltermeier@gmail.com <johannes.faltermeier@gmail.com@c03250ec-a590-73f7-ba79-85b89e17e35f>
2011-12-05 16:57:19 +0000

    Preferences are now handled by a helper class
    git-svn-id: https://faap-feedme.googlecode.com/svn/trunk@20 c03250ec-a590-73f7-ba79-85b89e17e35f

---
+++ FeedMe/src/de/faap/feedme/util/IPreferences.java
@@ -0,0 +1,23 @@
+package de.faap.feedme.util;
+
+public interface IPreferences {
+    public int[] getCheckedButtons();
+
+    public void saveCheckedButtons(int[] checkedButtons);
+
+    public String[] getType();
+
+    public void saveType(String[] type);
+
+    public String[] getEffort();
+
+    public void saveEffort(String[] effort);
+
+    public String[] getCuisine();
+
+    public void saveCuisine(String[] cuisine);
+
+    public String[] getWeek();
+
+    public void saveWeek(String[] week);
+}
---
+++ FeedMe/src/de/faap/feedme/util/Preferences.java
@@ -0,0 +1,102 @@
+package de.faap.feedme.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class Preferences implements IPreferences {
+
+    private static final String PREFS = "de.faap.FeedMe_preferences";
+
+    private SharedPreferences preferences;
+    private Editor editor;
+
+    public Preferences(Context context) {
+	preferences = context.getSharedPreferences(PREFS, 0);
+	editor = preferences.edit();
+    }
+
+    @Override
+    public int[] getCheckedButtons() {
+	int[] checkedButtons = new int[7];
+	for (int i = 0; i < checkedButtons.length; i++) {
+	    checkedButtons[i] = preferences.getInt("radioGroup" + i, -1);
+	}
+	return checkedButtons;
+    }
+
+    @Override
+    public void saveCheckedButtons(int[] checkedButtons) {
+	for (int i = 0; i < checkedButtons.length; i++) {
+	    editor.putInt("radioGroup" + i, checkedButtons[i]);
+	}
+    }
+
+    @Override
+    public String[] getType() {
+	String[] type = new String[preferences.getInt("type", 0)];
+	for (int i = 0; i < type.length; i++) {
+	    type[i] = preferences.getString("type" + i, "");
+	}
+	return type;
+    }
+
+    @Override
+    public void saveType(String[] type) {
+	editor.putInt("type", type.length);
+	for (int i = 0; i < type.length; i++) {
+	    editor.putString("type" + i, type[i]);
+	}
+    }
+
+    @Override
+    public String[] getEffort() {
+	String[] effort = new String[preferences.getInt("effort", 0)];
+	for (int i = 0; i < effort.length; i++) {
+	    effort[i] = preferences.getString("effort" + i, "");
+	}
+	return effort;
+    }
+
+    @Override
+    public void saveEffort(String[] effort) {
+	editor.putInt("effort", effort.length);
+	for (int i = 0; i < effort.length; i++) {
+	    editor.putString("effort" + i, effort[i]);
+	}
+    }
+
+    @Override
+    public String[] getCuisine() {
+	String[] cuisine = new String[preferences.getInt("cuisine", 0)];
+	for (int i = 0; i < cuisine.length; i++) {
+	    cuisine[i] = preferences.getString("cuisine" + i, "");
+	}
+	return cuisine;
+    }
+
+    @Override
+    public void saveCuisine(String[] cuisine) {
+	editor.putInt("cuisine", cuisine.length);
+	for (int i = 0; i < cuisine.length; i++) {
+	    editor.putString("cuisine" + i, cuisine[i]);
+	}
+    }
+
+    @Override
+    public String[] getWeek() {
+	String[] week = new String[7];
+	for (int i = 0; i < week.length; i++) {
+	    week[i] = preferences.getString("week" + i, "");
+	}
+	return week;
+    }
+
+    @Override
+    public void saveWeek(String[] week) {
+	for (int i = 0; i < week.length; i++) {
+	    editor.putString("week" + i, week[i]);
+	}
+    }
+
+}
