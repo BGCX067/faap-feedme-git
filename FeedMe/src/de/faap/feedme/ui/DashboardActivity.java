8cc84f6f481b826f00e887fc0d7f2985eaf2e440
johannes.faltermeier@gmail.com <johannes.faltermeier@gmail.com@c03250ec-a590-73f7-ba79-85b89e17e35f>
2011-12-18 15:37:08 +0000

    Added a lot of icons. fixed dashboard so it works in landscape mode
    git-svn-id: https://faap-feedme.googlecode.com/svn/trunk@33 c03250ec-a590-73f7-ba79-85b89e17e35f

--- FeedMe/src/de/faap/feedme/ui/DashboardActivity.java
+++ FeedMe/src/de/faap/feedme/ui/DashboardActivity.java
@@ -17,8 +17,8 @@ public class DashboardActivity extends ActionBarActivity {
 
     public static final String ACTIONBAR_TITLE = "title";
     public static final String ACTIONBAR_ICON = "icon";
-    public static final int iconPlan = R.drawable.ic_home; // TODO anpassen
-    public static final int iconRecipes = R.drawable.ic_home; // TODO anpassen
+    public static final int iconPlan = R.drawable.ic_action_planer;
+    public static final int iconRecipes = R.drawable.ic_action_recipes;
 
     /** Called when the activity is first created. */
     @Override
