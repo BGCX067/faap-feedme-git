f703072da7aabe91268a3daa5c60ea29ca3345d5
Alexander Aumann <a.aumann@web.de>
2012-03-02 01:32:02 +0100

    Minor DatabaseHelper count-increment. Pizza still in wrong category!!!

--- FeedMe/src/de/faap/feedme/provider/RecipeDatabaseHelper.java
+++ FeedMe/src/de/faap/feedme/provider/RecipeDatabaseHelper.java
@@ -15,7 +15,7 @@ public class RecipeDatabaseHelper extends SQLiteOpenHelper {
     }
 
     private static final String DATABASE_NAME = "Recipe_database";
-    private static final int DATABASE_VERSION = 18;
+    private static final int DATABASE_VERSION = 19;
 
     public RecipeDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
